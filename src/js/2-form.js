26887ac7222d55dde18a5d98fb778a9b44928bb1
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2025-02-13 22:21:19 +0200

    je

---
+++ src/js/2-form.js
@@ -0,0 +1,26 @@
+const a = document.querySelector("form");
+const localStorageKey = "feedback-form-state";
+let formData = JSON.parse(localStorage.getItem(localStorageKey)
+) || { email: "", message: "" };
+
+
+a.elements.email.value = formData.email;
+a.elements.message.value = formData.message;
+
+
+
+a.addEventListener('input', e => {
+    formData.email = e.currentTarget.elements.email.value;
+    formData.message = e.currentTarget.elements.message.value;
+
+    console.log(formData);
+
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+    console.log(formData);
+
+
+
+});
+
+
+
